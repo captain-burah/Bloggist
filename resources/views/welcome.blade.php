d803c9cc06220b28e190253ebf5b51db65ffda37
captain_burah <cptburah@gmail.com>
2020-07-03 11:13:15 +0530

    commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,11 +5,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         
+        <!-- CSRF Token -->
+        <meta name="csrf-token" content="{{ csrf_token() }}">
+        <script> window.laravel = { csrfToken: '{{ csrf_token() }}' } </script>
         <title>Venture 2021</title>
-        <link href="{{ asset('css/app.css') }}" type="style/css" rel="stylesheet">
+        
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Raleway:100,600" rel="stylesheet" type="text/css">
-
+        <link rel="stylesheet" href="css/app.css">
         <!-- Styles -->
         <style>
             a,
